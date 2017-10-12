3869558598c918e47b8170a5ca7fe0b57a4de61d
Aureli <moliner5765@gmail.com>
2017-10-12 13:09:08 +0200

    Add files via upload

---
+++ biblioteca/biblioteca/Nav.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace biblioteca
+{
+    class Nav
+    {
+        private string llegir(string sURL)
+        {
+            string var_pagina = string.Empty;
+            // Capturem la pàgina
+            try
+            {
+                System.Net.WebClient client = new System.Net.WebClient();
+                Byte[] pagedata = client.DownloadData(sURL);
+                var_pagina = System.Text.Encoding.GetEncoding("iso-8859-1").GetString(pagedata);                
+            }
+            catch (System.Net.WebException webEx)
+            {
+                if (webEx.Status == System.Net.WebExceptionStatus.ConnectFailure)
+                {
+                    System.Windows.Forms.MessageBox.Show("Are you behind a firewall? If so, go through the proxy server");
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Ha d'haver passat alguna cosa:" + Environment.NewLine + webEx.Message);
+                }
+            }
+            return var_pagina;
+        }
+    }
+}
---
+++ biblioteca/biblioteca/bDallioli.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+
+namespace biblioteca
+{
+    class bDallioli
+    {
+        public int siNoEstaenAlliInsertar()         {
+            int iNR = -1;
+            using (OleDbConnection cnn = new OleDbConnection(conString))
+            {
+                string sql = "SELECT * FROM  alli WHERE data = ? and cognoms = ?";
+                OleDbCommand cmd = new OleDbCommand(sql, cnn);
+                cmd.Parameters.AddWithValue("@data", OleDbType.WChar).Value = f.data;
+                cmd.Parameters.AddWithValue("@cognoms", OleDbType.WChar).Value = f.cognoms;
+                OleDbDataAdapter adapter = new OleDbDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataTable table = new DataTable();
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                adapter.Fill(table);
+                iNR = table.Rows.Count;
+            }
+            if (iNR == 0)
+            {
+
+                using (OleDbConnection cnn = new OleDbConnection(conString))             {
+                    string query = "INSERT INTO alli (numFormulari, " +
+                            "adre, codicentre,codipostal, cognoms, c" +
+                            "ontrol, data, datanaix, dni, entitat, i" +
+                            "ban, mail,nom, nomipoblacio, numero,obs" +
+                            "ervacions, oficina, poblacio, sitadmin," +
+                            " telefon, territori, tmovil) VALUES " +
+                            "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+                    OleDbCommand cmd = new OleDbCommand(query, cnn);
+                    cnn.Open();
+                    cmd.Parameters.AddWithValue("@numFormulari", OleDbType.Integer).Value = f.numFormulari;
+                    cmd.Parameters.AddWithValue("@adre", OleDbType.WChar).Value = f.adre;
+                    cmd.Parameters.AddWithValue("@codicentre", OleDbType.WChar).Value = f.codicentre;
+                    cmd.Parameters.AddWithValue("@codipostal", OleDbType.WChar).Value = f.codipostal;
+                    cmd.Parameters.AddWithValue("@cognoms", OleDbType.WChar).Value = f.cognoms;
+                    cmd.Parameters.AddWithValue("@control", OleDbType.WChar).Value = f.control;
+                    cmd.Parameters.AddWithValue("@data", OleDbType.WChar).Value = f.data;
+                    cmd.Parameters.AddWithValue("@datanaix", OleDbType.WChar).Value = f.datanaix;
+                    cmd.Parameters.AddWithValue("@dni", OleDbType.WChar).Value = f.dni;
+                    cmd.Parameters.AddWithValue("@entitat", OleDbType.WChar).Value = f.entitat;
+                    cmd.Parameters.AddWithValue("@iban", OleDbType.WChar).Value = f.iban;
+                    cmd.Parameters.AddWithValue("@mail", OleDbType.WChar).Value = f.mail;
+                    cmd.Parameters.AddWithValue("@nom", OleDbType.WChar).Value = f.nom;
+                    cmd.Parameters.AddWithValue("@nomipoblacio", OleDbType.WChar).Value = f.nomipoblacio;
+                    cmd.Parameters.AddWithValue("@numero", OleDbType.WChar).Value = f.numero;
+                    cmd.Parameters.AddWithValue("@observacions", OleDbType.WChar).Value = f.observacions;
+                    cmd.Parameters.AddWithValue("@oficina", OleDbType.WChar).Value = f.oficina;
+                    cmd.Parameters.AddWithValue("@poblacio", OleDbType.WChar).Value = f.poblacio;
+                    cmd.Parameters.AddWithValue("@sitadmin", OleDbType.WChar).Value = f.sitadmin;
+                    cmd.Parameters.AddWithValue("@telefon", OleDbType.WChar).Value = f.telefon;
+                    cmd.Parameters.AddWithValue("@territori", OleDbType.WChar).Value = f.territori;
+                    cmd.Parameters.AddWithValue("@tmovil", OleDbType.WChar).Value = f.tmovil;
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        System.Windows.Forms.MessageBox.Show(e.Message);
+                        throw;
+                    }
+                } else {
+                    contadv += 1;
+                }
+            }
+        }
+    }
+}
